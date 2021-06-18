7318068b13405043b37c0b66c407b3ffd680be69
P-Pix <Guillaume200202@gmail.com>
2021-06-18 17:30:12 +0200

    header

---
+++ import/Pltcpp.h
@@ -0,0 +1,27 @@
+/**
+ * @file Pltcpp.h
+ * @author Guillaume LEMONNIER
+ * @brief 
+ * @version 0.1
+ * @date 2021-06-18
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef DEF_PLTCPP
+#define DEF_PLTCPP
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+class Pltcpp
+{
+    private:
+
+    protected:
+
+    public:
+};
+
+#endif
